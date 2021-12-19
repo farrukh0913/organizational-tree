876c897c3e3d0ebaebac1e482c1a28dec6be0838
khurram <youthpassion.com@gmail.com>
2021-12-20 00:19:40 +0500

    code refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,17 @@ export class AppComponent implements OnInit {
   constructor() { }
 
   ngOnInit(){
-    // organizational tree
-    
+    // tree customization
     this.treeCustomization();
-    this.orgTree();
+    // load tree
+    this.loadTree();
   }
 
+// #region tree customization 
+
+  /**
+   * tree customization
+   */
   treeCustomization(){
     OrgChart.templates.ana.plus =
             '<rect x="0" y="0" width="36" height="36" rx="50" ry="50" fill="green" stroke="#aeaeae" stroke-width="1"></rect>'
@@ -29,7 +34,14 @@ export class AppComponent implements OnInit {
         '<text class="field_2" style="font-size: 14px;" fill="#ffffff" x="125" y="70" text-anchor="middle">{val}</text>';
   }
 
-  orgTree(){
+//#endregion
+
+// #region load tree 
+
+  /**
+   * load tree
+   */
+  loadTree(){
     const tree = document.getElementById('tree');
     if (tree) {
         var chart = new OrgChart(tree, {
@@ -44,13 +56,15 @@ export class AppComponent implements OnInit {
         chart.load([
   { id: 1, name: "Farrukh Shahzad", title: "Developer", salary:"Salary: 2000", img: "/assets/farrukh.png" },
                         { id: 2, pid: 1, name: "Madiha Yasir", title: "Sales Manager", salary:"Salary: 2000",  img: "https://cdn.balkan.app/shared/anim/1.gif" },
-                        { id: 3, pid: 1, name: "Adeel Meo", title: "Dev Manager", salary:"Salary: 1000",  img: "/assets/user2.svg" },
-                        { id: 4, pid: 2, name: "Ehaan Meo", title: "Sales", salary:"Salary: 3000",  img: "/assets/user2.svg" },
-                        { id: 5, pid: 2, name: "Fayha Khurram", title: "Sales", salary:"Salary: 4000",  img: "/assets/user2.svg" },
-                        { id: 6, pid: 3, name: "Haider Amir", title: "Developer", salary:"Salary: 5000",  img: "/assets/user2.svg" },
-                        { id: 7, pid: 3, name: "Amir Khan", title: "Developer", salary:"Salary: 8000",  img: "/assets/user2.svg" }
+                        { id: 3, pid: 1, name: "Adeel Meo", title: "Dev Manager", salary:"Salary: 1000",  img: "/assets/user.svg" },
+                        { id: 4, pid: 2, name: "Ehaan Meo", title: "Sales", salary:"Salary: 3000",  img: "/assets/user.svg" },
+                        { id: 5, pid: 2, name: "Fayha Khurram", title: "Sales", salary:"Salary: 4000",  img: "/assets/user.svg" },
+                        { id: 6, pid: 3, name: "Haider Amir", title: "Developer", salary:"Salary: 5000",  img: "/assets/user.svg" },
+                        { id: 7, pid: 3, name: "Amir Khan", title: "Developer", salary:"Salary: 8000",  img: "/assets/user.svg" }
         ]);
     }
   }
+
+  //#endregion
 
 }
